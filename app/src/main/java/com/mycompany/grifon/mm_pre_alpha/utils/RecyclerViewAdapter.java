eabb0918f75983c81f6b8f5aed8a5d07f099c4c5
grifon <Grinnbob>
2017-10-19 22:15:22 +0300

    корректно добавляется музыка в Storage and Databae, с лайками стена не доделана поправил LoginActivity, было некорректно

---
+++ app/src/main/java/com/mycompany/grifon/mm_pre_alpha/utils/RecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.mycompany.grifon.mm_pre_alpha.utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mycompany.grifon.mm_pre_alpha.R;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>  {
+
+    private List<String> mDataset;
+
+    // класс view holder-а с помощью которого мы получаем ссылку на каждый элемент
+    // отдельного пункта списка
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // наш пункт состоит только из одного TextView
+        public TextView mTextView;
+        // сюда добавлять другие вью
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.tv_recycler_item);
+        }
+    }
+
+    // Конструктор
+    public RecyclerViewAdapter(List<String> dataset) {
+        mDataset = dataset;
+    }
+
+    // Создает новые views (вызывается layout manager-ом)
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_item, parent, false);
+
+        // тут можно программно менять атрибуты лэйаута (size, margins, paddings и др.)
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Заменяет контент отдельного view (вызывается layout manager-ом)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position));
+    }
+
+    // Возвращает размер данных (вызывается layout manager-ом)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
