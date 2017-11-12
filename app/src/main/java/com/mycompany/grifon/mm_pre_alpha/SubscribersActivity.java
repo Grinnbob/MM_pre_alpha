e49ebec04a98b2726de574f03dcc798b6c39f85c
Vlad Duplyakin <DuplyakinV@gmail.com>
2017-11-12 11:41:31 +0300

    fix subscribers names; subscribers activity done;  TODO: same for subscriptions, save checkBox status

--- app/src/main/java/com/mycompany/grifon/mm_pre_alpha/SubscribersActivity.java
+++ app/src/main/java/com/mycompany/grifon/mm_pre_alpha/SubscribersActivity.java
@@ -8,27 +8,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mycompany.grifon.mm_pre_alpha.data.FirebasePathHelper;
 import com.mycompany.grifon.mm_pre_alpha.data.PlainUser;
 import com.mycompany.grifon.mm_pre_alpha.events.AllMyUsersEvent;
 import com.mycompany.grifon.mm_pre_alpha.events.Events;
+import com.mycompany.grifon.mm_pre_alpha.events.MyProfileEvent;
+import com.mycompany.grifon.mm_pre_alpha.events.UserProfileEvent;
 import com.mycompany.grifon.mm_pre_alpha.ui.subscribers.SubscribersAdapter;
 import com.mycompany.grifon.mm_pre_alpha.utils.EBActivity;
+import com.mycompany.grifon.mm_pre_alpha.utils.domain.Profile;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class SubscribersActivity extends EBActivity {
 
     private Toolbar toolbar;
-    private Intent intentMusic;
-    private Intent intentNews;
-    private Intent intentProfile;
 
     RecyclerView rvSubscribers;
     SubscribersAdapter subscribersAdapter;
+
+    private PlainUser plainUser;
+    FirebaseUser user;
+    Profile profile;//тот чел на которого ткнули чтобы посмотреть
+    Profile myProfile;//наш профиль
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +55,43 @@ public class SubscribersActivity extends EBActivity {
         rvSubscribers.setLayoutManager(layoutManager);
         rvSubscribers.setAdapter(subscribersAdapter);
         //FirebasePathHelper.requestAllUsers();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        plainUser = (PlainUser) intent.getSerializableExtra("user");
+        if (plainUser == null) { //попали сюда не ткнув на какого-то пользователя, а ткнули со своего профиля
+            plainUser = new PlainUser(user);
+        }
+        FirebasePathHelper.getMyProfile(user.getUid());
+        FirebasePathHelper.getUserProfile(plainUser.getUuid());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMyProfileData(MyProfileEvent evt) {
+        //   plainUser = new PlainUser(user.getDisplayName(), user.getUid());
+        myProfile = evt.getProfile();
+        if (myProfile != null && myProfile.getUuid().equals(plainUser.getUuid())) {
+            Map<String, PlainUser> subscribers = myProfile.getSubscribers();
+            subscribersAdapter.replaceData(new ArrayList<>(subscribers.values()));
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onUserProfileData(UserProfileEvent evt) {
+        if (plainUser == null) {
+            plainUser = new PlainUser(user);
+        }
+        profile = evt.getProfile();
+        if (profile != null && profile.getUuid().equals(plainUser.getUuid())) {
+            Map<String, PlainUser> subscribers = profile.getSubscribers();
+            subscribersAdapter.replaceData(new ArrayList<>(subscribers.values()));
+        }
 
     }
 
@@ -70,17 +118,17 @@ public class SubscribersActivity extends EBActivity {
             case R.id.subscribers:
                 break;
             case R.id.music:
-                intentMusic = new Intent(this, MusicActivity.class);
+                Intent intentMusic = new Intent(this, MusicActivity.class);
                 startActivity(intentMusic);
                 this.finish();
                 break;
             case R.id.news:
-                intentNews = new Intent(this, NewsActivity.class);
+                Intent intentNews = new Intent(this, NewsActivity.class);
                 startActivity(intentNews);
                 this.finish();
                 break;
             case R.id.profile:
-                intentProfile = new Intent(this, ProfileActivity.class);
+                Intent intentProfile = new Intent(this, ProfileActivity.class);
                 startActivity(intentProfile);
                 this.finish();
                 break;
